6bb9dade6c011e5782376e60941325e727efc603
Vados61 <pvild3d@gmail.com>
2022-11-13 09:52:11 +0300

    fix actions 3

--- sample_proj/settings.py
+++ sample_proj/settings.py
@@ -79,12 +79,12 @@ WSGI_APPLICATION = 'sample_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'PORT': '5432'
     }
 }
 
